4e30ecb41e034e9386257301f768b2c7f335187f
abauer361 <abauer@chapman.edu>
2025-05-14 13:25:41 -0700

    Trying this

--- nike-website/src/sections/Hero.jsx
+++ nike-website/src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import Button from "../components/Button"
 import { arrowRight } from "../assets/icons"
 import { shoes, statistics } from "../constants"
 import { bigShoe1 } from "../assets/images";
-import Shoecard from "../components/Shoecard";
+import Shoecard from "../components/ShoeCard";
 import { useState } from 'react'
 
 const Hero = () => {
